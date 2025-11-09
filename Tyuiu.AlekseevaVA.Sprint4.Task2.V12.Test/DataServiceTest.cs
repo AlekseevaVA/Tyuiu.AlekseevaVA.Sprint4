2d2f1036edb118c2a72eebda3077df395e22f9cc
AlekseevaVA <alekseevavria@mail.ru>
2025-11-09 12:04:11 +0500

    Task2

---
+++ Tyuiu.AlekseevaVA.Sprint4.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlekseevaVA.Sprint4.Task2.V12.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint4.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 5, 7, 4, 9, 6, 5, 8, 7, 9, 4, 5, 7, 9, 5 };
+
+            int wait = 68;
+
+            int res = ds.Calculate(array);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
