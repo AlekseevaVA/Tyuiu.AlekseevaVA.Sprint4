c6b5025019c556cb79641db4978e774262e16317
AlekseevaVA <alekseevavria@mail.ru>
2025-11-09 14:57:09 +0500

    Task5

---
+++ Tyuiu.AlekseevaVA.Sprint4.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AlekseevaVA.Sprint4.Task5.V30.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint4.Task5.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5]
+            {
+               {-1,-1,-1,-1,-1 },
+               {-1,-1,-1,-1,1 },
+               {-1,-1,2,-1,-1 },
+               {-1,-1,-1,-1,-1 },
+               {-1,-1,-1,-1,4 }
+            };
+            int res = ds.Calculate(matrix);
+            int wait = 7;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
