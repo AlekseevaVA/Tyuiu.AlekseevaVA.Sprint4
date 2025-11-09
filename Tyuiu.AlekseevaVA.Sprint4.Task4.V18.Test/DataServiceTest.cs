a2e9feb2798387f2e830336faa74ec36f46ab1d3
AlekseevaVA <alekseevavria@mail.ru>
2025-11-09 14:25:20 +0500

    Task4

---
+++ Tyuiu.AlekseevaVA.Sprint4.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.AlekseevaVA.Sprint4.Task4.V18.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint4.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5] {{4, 7, 4, 5, 4 },
+                                           {4, 4, 5, 6, 7 },
+                                           {7, 5, 6, 6, 6 },
+                                           { 6, 7, 6, 6, 4},
+                                           { 5, 6, 6, 6, 5} };
+
+            int[,] wait = new int[5, 5] {   { 4, 0, 4, 0, 4 },
+                                            { 4, 4, 0, 6, 0 },
+                                            { 0, 0, 6, 6, 6 },
+                                            { 6, 0, 6, 6, 4 },
+                                            { 0, 6, 6, 6, 0 } };
+            var rez = ds.Calculate(matrix);
+
+            CollectionAssert.AreEqual(wait, rez);
+        }
+    }
+}
