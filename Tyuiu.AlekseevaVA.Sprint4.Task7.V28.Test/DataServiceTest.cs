10b2511125035b73984e69dec3e51c5190130ca0
AlekseevaVA <alekseevavria@mail.ru>
2025-11-09 17:06:42 +0500

    Task7

---
+++ Tyuiu.AlekseevaVA.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AlekseevaVA.Sprint4.Task7.V28.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 5;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "623351179845632";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4608;
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
